c2fc2639f0bde635c162c833b5833d44a60a7427
Midas van Soest <midasvsoest@gmail.com>
2025-04-22 15:26:29 +0200

    Setup navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 function Footer () {
     return (
-        <>
-        
-        </>
+        <footer>
+            <a href="https://github.com/Horrusius/mini-project-react-app">Link to repo</a>
+        </footer>
     );
 }
 

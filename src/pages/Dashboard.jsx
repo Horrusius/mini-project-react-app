ad15b539e707336fdb857f11e1fc4f7d8356f8e6
Midas van Soest <midasvsoest@gmail.com>
2025-04-30 16:46:32 +0200

    edit: removed unnecesary elements from Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,10 @@
 import RentalList from "../components/RentalList";
-import CreateRental from "./CreateRental";
-import EditRental from "./EditRental";
 
-function Dashboard({ rentalsArr, onDelete, createRental, editRental }) {
+function Dashboard({ rentalsArr, onDelete}) {
   return (
     <div>
       <h1>This is the Dashboard</h1>
-      <RentalList rentalsArr={rentalsArr} onDelete={onDelete} editRental={editRental} />
+      <RentalList rentalsArr={rentalsArr} onDelete={onDelete}/>
     </div>
   );
 }
